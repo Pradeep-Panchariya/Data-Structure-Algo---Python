180e9b13f80ead9a433961a59067b756c7276eb4
My name <me@example.com>
2021-12-28 01:05:26 +0530

    Basic running time complexity for LinkedList

---
+++ LinkedListPython/Basic LInked List Theory.py
@@ -0,0 +1,14 @@
+# Insertion and deletion in the linked list : O(1) constant Time complexity
+#For accessing the arbitary element : O(N) running time complexity (Linear).
+
+#Insert itema at beginning : O(1)  vs array O(N)
+#remove item at the beginning : O(1) vs array O(N)
+#Insert item at the end of the linked list : O(N) vs array O(1)
+#To remove item at the arbitary location : O(N) vs array O(N)
+
+#Conclusion
+# Manipulate the first item (add or remove)
+# O(1)
+
+# Manipulating arbitary item:
+# O(N)

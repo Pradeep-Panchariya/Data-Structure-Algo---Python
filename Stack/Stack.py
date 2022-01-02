defeb6279a281a19b2acdf680e5515743f8887ca
My name <me@example.com>
2022-01-02 22:34:40 +0530

    Stack data structure implementation

---
+++ Stack/Stack.py
@@ -0,0 +1,62 @@
+"""
+@Author : Pradeep Panchariya
+@Email : panchariya11@gmail.com
+"""
+
+#Implementing the stack data structure
+#LIFO : Last in fast out
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    #Inserting the item to the stack : at the end
+    #O(1)  : Running time complexity
+    def push(self, data):
+        self.stack.append(data)
+
+    #removing the item from the stack and return the removed value : at the end
+    #O(1) : Running time complexity
+    def pop(self):
+        #checking whether stack is empty or not to avoid exception
+        if self.is_empty():
+            return -1
+        data = self.stack.pop() # self.stack[-1] also valid to use
+        return data
+
+    #getting the last item from the stack
+    #O(1) : running time complexity
+    def peek(self):
+        if self.is_empty():
+            return -1
+        return self.stack[-1]
+
+    #check whether stack is empty or not
+    def is_empty(self):
+        return self.stack == []
+
+    #getting the size (total element) of the stack
+    def size(self):
+        return len(self.stack)
+
+    #print the Stack
+    def print_data(self):
+        print(self.stack)
+#initialize the stack object
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+#print
+stack.print_data()
+#getting the size
+print(stack.size())
+#check whether empty or not True or False
+print(stack.is_empty())
+#removing the item
+last_element = stack.pop()
+#print again
+stack.print_data()
+#getting the last element
+lst_ele = stack.peek()

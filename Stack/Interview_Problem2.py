df7cde68beb0278a93eb56a7372290e5ab63a5ed
Pradeep <panchariya11@gmail.com>
2022-01-03 12:17:05 +0530

    Updated - interview Q - Implement Q using stack recursive method.

--- Stack/Interview_Problem2.py
+++ Stack/Interview_Problem2.py
@@ -19,20 +19,24 @@ class QueueUsingStackRecursion:
             return self.queue.pop()
 
         #getting the item
+        if self.queue==[]:
+            return -1
         item = self.queue.pop()
 
         #remove all the item
-        recursive = self.dequeue()
+        dequeued_item = self.dequeue()
 
         #append the item in the reverse order : queue FIFO
         self.queue.append(item)
 
-        return self.queue.pop()
+
+        return self.queue
 
 
 if __name__ == '__main__':
     Q = QueueUsingStackRecursion()
     for i in range(2,20,2):
         Q.enqueue(i)
+    print(Q.queue)
     item = Q.dequeue()
-    print(item)
+    print(item,end=",")
